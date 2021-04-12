3481f1e470ebb94962912513ae36feba5f578aec
Kyle King <KyleKing@users.noreply.github.com>
2021-04-12 08:33:29 -0400

    build: update setup.py

--- not_on_pypi/__init__.py
+++ not_on_pypi/__init__.py
@@ -1,15 +1,1 @@
-"""Raise error to indicate that the wrong package was installed."""
-
-
-class WrongPackageInstalledError(RuntimeError):
-    """More specific error."""
-
-    pass
-
-
-raise WrongPackageInstalledError("""
-This package was downloaded from the public pypi.org repository,
-but should have been downloaded from an internal repository.
-
-Please update the installer's configuration and download from the proper repository
-""")
+"""Do nothing."""
--- setup.py
+++ setup.py
@@ -1,21 +1,56 @@
-# Generated with poetry run dephell deps convert --from-format poetry --from-path pyproject.toml --to-format setuppy --to-path setup.py
+"""Setup File."""
 
+from pathlib import Path
 from setuptools import setup
+from setuptools.command.install import install
+
+PACKAGE_NAME = 'common_app'
+"""Modify the package name here which is to be seen on PyPi."""
+
+AUTHOR = 'Kyle King'
+AUTHOR_EMAIL = 'dev.act.kyle@gmail.com'
+
+# --------------------------------------------------------------------------------------
+
+
+class WrongPackageInstalledError(RuntimeError):
+    """More specific error."""
+
+    pass
+
+
+class RaiseErrorPreInstall(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+
+    def run(self):
+        raise WrongPackageInstalledError("""
+This package was downloaded from the public pypi.org repository, but
+should have been downloaded from an internal repository.
+
+Please update the installer's configuration and download from the proper index-url
+""")
+
 
 
 if __name__ == '__main__':
     setup(
-        long_description='',
-        name='common_app',
+        name=PACKAGE_NAME,
         version='0.0.0a1',
-        description='Reserved package name',
-        python_requires='*',
-        project_urls={"documentation": "https://github.com/KyleKing/not-on-pypi", "repository": "https://github.com/KyleKing/not-on-pypi"},
-        author='Kyle King',
-        author_email='dev.act.kyle@gmail.com',
-        packages=['not_on_pypi'],
-        package_dir={"": "."},
-        package_data={},
-        install_requires=[],
-        extras_require={"dev": ["dephell==0.*,>=0.8.3"]},
+        packages=[PACKAGE_NAME],
+        description = 'Reserved package name',
+        long_description = Path('README.md').read_text(),
+        long_description_content_type='text/markdown',
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url='https://github.com/KyleKing/not-on-pypi',
+        license = 'Unlicense',
+        classifiers=['License :: Public Domain'],
+        cmdclass={
+            'install': RaiseErrorPreInstall,
+        },
+        # repository = 'https://github.com/KyleKing/not-on-pypi',
+        # documentation = 'https://github.com/KyleKing/not-on-pypi',
+        # 'Bug Tracker' = 'https://github.com/KyleKing/not-on-pypi/issues',
+        # include = [ 'LICENSE.md',],
+        # scripts = [],
     )
